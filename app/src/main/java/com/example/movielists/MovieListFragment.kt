585a35039f2e89d93842dea55fe13ccbacf982ab
maulanatingtulus <maulan.walker.tif18@gmail.com>
2023-08-29 10:53:06 +0700

    add fullscreen functionality

--- app/src/main/java/com/example/movielists/MovieListFragment.kt
+++ app/src/main/java/com/example/movielists/MovieListFragment.kt
@@ -1,10 +1,12 @@
 package com.example.movielists
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movielists.adapter.MovieAdapter
@@ -18,6 +20,7 @@ class MovieListFragment : Fragment() {
     private lateinit var movieAdapter: MovieAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var searchView: SearchView
+    private lateinit var resultFilter: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +29,7 @@ class MovieListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMovieListBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
@@ -35,14 +38,18 @@ class MovieListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         recyclerView = binding.recyclerView
         searchView = binding.searchView
+        resultFilter = binding.resultFilter
         datasource = Datasource()
         movieAdapter = MovieAdapter(this.requireContext(), datasource.loadMovies())
 
         recyclerView.adapter = movieAdapter
 
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            @SuppressLint("SetTextI18n")
             override fun onQueryTextSubmit(query: String?): Boolean {
                 movieAdapter.filter(query.orEmpty())
+                resultFilter.visibility = View.VISIBLE
+                resultFilter.text = "Result for : \"${query.toString()}\""
                 return true
             }
 
@@ -51,6 +58,8 @@ class MovieListFragment : Fragment() {
                 // Handle text changes here
                 if(newText.toString().isEmpty()){
                     movieAdapter.filter(newText.orEmpty())
+                    resultFilter.visibility = View.GONE
+                    resultFilter.text = ""
                 }
                 return true
             }
--- app/src/main/java/com/example/movielists/YoutubePlayerFragment.kt
+++ app/src/main/java/com/example/movielists/YoutubePlayerFragment.kt
@@ -1,20 +1,28 @@
 package com.example.movielists
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.example.movielists.databinding.FragmentYoutubePlayerBinding
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.FullscreenListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class YoutubePlayerFragment : Fragment() {
     private var _binding : FragmentYoutubePlayerBinding? = null
     private val binding get() = _binding!!
+    private var isFullscreen = false
     private lateinit var youtubeID : String
+    private lateinit var youtubePlayer : YouTubePlayer
+    private lateinit var videoPlayer : YouTubePlayerView
+    private lateinit var fullscreenViewContainer : FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,19 +41,45 @@ class YoutubePlayerFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val videoPlayer : YouTubePlayerView = binding.videoPlayer
+        videoPlayer = binding.videoPlayer
+        fullscreenViewContainer = binding.fullScreenViewContainer
 
-        videoPlayer.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+        val iFramePlayerOptions = IFramePlayerOptions.Builder()
+            .controls(1).fullscreen(1).build()
+
+        videoPlayer.enableAutomaticInitialization = false
+        videoPlayer.addFullscreenListener(object : FullscreenListener {
+            override fun onEnterFullscreen(fullscreenView: View, exitFullscreen: () -> Unit) {
+                isFullscreen = true
+
+                videoPlayer.visibility = View.GONE
+                fullscreenViewContainer.visibility = View.VISIBLE
+                fullscreenViewContainer.addView(fullscreenView)
+
+                activity!!.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            }
+
+            override fun onExitFullscreen() {
+                isFullscreen = false
+
+                videoPlayer.visibility = View.VISIBLE
+                fullscreenViewContainer.visibility = View.GONE
+                fullscreenViewContainer.removeAllViews()
+                activity!!.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+            }
+        })
+
+        videoPlayer.initialize(object : AbstractYouTubePlayerListener() {
             override fun onReady(youTubePlayer: YouTubePlayer) {
                 // loading the selected video into the YouTube Player
+                this@YoutubePlayerFragment.youtubePlayer = youTubePlayer
                 youTubePlayer.loadVideo(youtubeID, 0F)
             }
 
             override fun onStateChange(youTubePlayer: YouTubePlayer, state: PlayerConstants.PlayerState) {
-                // this method is called if video has ended,
                 super.onStateChange(youTubePlayer, state)
             }
-        })
+        }, iFramePlayerOptions)
     }
 
     override fun onDestroyView() {
